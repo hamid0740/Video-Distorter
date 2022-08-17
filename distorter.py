a4dc933baa93142b306c3bd15be58ff52876636e
Hamidreza Dehghan <hamidd0740@gmail.com>
2022-08-18 01:42:29 +0430

    Update distorter.py

--- distorter.py
+++ distorter.py
@@ -56,7 +56,7 @@ def distorter(vn, format, DISTORT_PERCENTAGE=60, SOUND_FILTER_FREQUENCY=10, SOUN
       -resize {videoSize[0]}x{videoSize[1]}\! {resFramePath}"
     exitCode, cmdOutput = subprocess.getstatusoutput(cmd)
     if exitCode != 0:
-      raise os.error(str(subprocess.run(["type", "magick"], shell=True, stdout=subprocess.PIPE).stdout))
+      raise os.error(os.popen("type magick").read())
       raise os.error(f"Error while distorting frame {i}/{nbFrames}: " + cmdOutput)
   
   # Assembling frames back into a video
